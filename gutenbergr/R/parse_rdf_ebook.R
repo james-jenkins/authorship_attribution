f7622ccde9fcfa216e2aeaf946f9e072b0bb5b70
James Jenkins <james.jenkins9@gmail.com>
2018-03-06 18:57:42 +0000

    Fix ebook parser namespaces.

--- gutenbergr/R/parse_rdf_ebook.R
+++ gutenbergr/R/parse_rdf_ebook.R
@@ -1,10 +1,15 @@
 parse_rdf_ebook <- function(node) {
-  children <- xml2::xml_children(node)
 
-  formats <- xml2::xml_find_all(children, "//dcterms:hasFormat")
+  formats <- xml2::xml_find_all(node, "dcterms:hasFormat")
 
-  other_names <- xml2::xml_name(children, ns = xml_ns(children))
+  ns <- xml2::xml_ns(node)
+
+  other_names <- xml2::xml_name(xml2::xml_children(node), ns)
   other_names <- other_names[other_names != "dcterms:hasFormat"]
-  other_nodes <- xml2::xml_find_all(children, paste0("//", paste(other_names, collapse = " | //")))
-  setNames(lapply(other_nodes, xml2::xml_text), other_names)
+  print_names <- gsub(".*[:]", "", other_names)
+
+  other_nodes <- xml2::xml_find_all(node, paste(other_names, collapse = " | "), ns = ns)
+
+
+  setNames(lapply(other_nodes, xml2::xml_text), print_names)
 }
